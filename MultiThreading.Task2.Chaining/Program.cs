d15c7d9bf8247d88ae25d42d6fb3c4c5243d704f
Kemal Maralov <Kemal_Maralov@epam.com>
2023-10-23 16:45:36 +0600

    Completed chaining task

--- MultiThreading.Task2.Chaining/Program.cs
+++ MultiThreading.Task2.Chaining/Program.cs
@@ -6,6 +6,8 @@
  * Fourth Task – calculates the average value. All this tasks should print the values to console.
  */
 using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task2.Chaining
 {
@@ -22,8 +24,58 @@ namespace MultiThreading.Task2.Chaining
             Console.WriteLine();
 
             // feel free to add your code
+            RunChaining();
 
             Console.ReadLine();
+        }
+
+        static async Task RunChaining()
+        {
+            await Task.Run(() => CreateRandomArray(10)).ContinueWith(x => MultiplyArray(x.Result))
+                                                       .ContinueWith(x => SortAscending(x.Result))
+                                                       .ContinueWith(x => CalculateAverage(x.Result));
+        }
+
+        static int[] CreateRandomArray(int size)
+        {
+            var rnd = new Random();
+            var array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rnd.Next(7, 77);
+            }
+
+            Console.WriteLine($"Initial array is [{string.Join(", ", array)}].");
+            return array;
+        }
+
+        static int[] MultiplyArray(int[] array)
+        {
+            var rnd = new Random();
+            var val = rnd.Next(2, 10);
+
+            int[] result = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                result[i] = array[i] * val;
+            }
+
+            Console.WriteLine($"This array multiplied by {val} is [{string.Join(", ", result)}].");
+            return result;
+        }
+
+        static int[] SortAscending(int[] array)
+        {
+            Array.Sort(array);
+
+            Console.WriteLine($"This array sorted by ascending is [{string.Join(", ", array)}].");
+            return array;
+        }
+
+        static void CalculateAverage(int[] array)
+        {
+            Console.WriteLine($"The average of the final array is {array.Average()}");
         }
     }
 }
