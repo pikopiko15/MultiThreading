ff27d8f97b880f1394a8b5c029c097fe77bb270e
Kemal Maralov <Kemal_Maralov@epam.com>
2023-10-24 18:08:31 +0600

    completed join threads task

--- MultiThreading.Task4.Threads.Join/Program.cs
+++ MultiThreading.Task4.Threads.Join/Program.cs
@@ -10,11 +10,14 @@
  */
 
 using System;
+using System.Threading;
 
 namespace MultiThreading.Task4.Threads.Join
 {
     class Program
     {
+        static Semaphore semaphore = new Semaphore(10, 100);
+
         static void Main(string[] args)
         {
             Console.WriteLine("4.	Write a program which recursively creates 10 threads.");
@@ -27,8 +30,44 @@ namespace MultiThreading.Task4.Threads.Join
             Console.WriteLine();
 
             // feel free to add your code
+            int initialNumber = 10;
+
+            Thread thread = new Thread(DecrementAndPrintWithJoin);
+            thread.Start(initialNumber);
+            thread.Join();
+
+            ThreadPool.QueueUserWorkItem(DecrementAndPrintWithSemaphore, initialNumber);
 
             Console.ReadLine();
+        }
+
+        static void DecrementAndPrintWithJoin(object state)
+        {
+            int number = (int)state;
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: {number}");
+            number--;
+
+            if (number > 0)
+            {
+                Thread newThread = new Thread(DecrementAndPrintWithJoin);
+                newThread.Start(number);
+                newThread.Join();
+            }
+        }
+
+        static void DecrementAndPrintWithSemaphore(object state)
+        {
+            int number = (int)state;
+            semaphore.WaitOne();
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: {number}");
+            number--;
+
+            if (number > 0)
+            {
+                ThreadPool.QueueUserWorkItem(DecrementAndPrintWithSemaphore, number);
+            }
+
+            semaphore.Release();
         }
     }
 }
