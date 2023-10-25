cfac711fbcfc8bbc3684bb7173d49727007a56f5
Kemal Maralov <Kemal_Maralov@epam.com>
2023-10-25 16:03:58 +0600

    completed shared collection task

--- MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,6 +5,8 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
@@ -18,6 +20,35 @@ namespace MultiThreading.Task5.Threads.SharedCollection
             Console.WriteLine();
 
             // feel free to add your code
+            List<int> sharedCollection = new List<int>();
+
+            Semaphore writeConcurrentOperation = new Semaphore(0, 1);
+            writeConcurrentOperation.Release();
+
+            Semaphore readConcurrentOperation = new Semaphore(0, 1);
+
+            Thread writeToCollection = new Thread(() =>
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    writeConcurrentOperation.WaitOne();
+                    sharedCollection.Add(i);
+                    readConcurrentOperation.Release();
+                }
+            });
+
+            Thread readFromCollection = new Thread((arg) =>
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    readConcurrentOperation.WaitOne();
+                    Console.WriteLine("[" + string.Join(", ", sharedCollection) + "]");
+                    writeConcurrentOperation.Release();
+                }
+            });
+
+            writeToCollection.Start();
+            readFromCollection.Start();
 
             Console.ReadLine();
         }
